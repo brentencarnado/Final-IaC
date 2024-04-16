4a4d32d9884dd8deac0d82a8c1dfffd8d4834bc3
Chowdhury611 <156258299+Chowdhury611@users.noreply.github.com>
2024-04-15 20:31:20 -0400

    Update main.tf
    Added secret manually in the code as it was preventing me to push my local code

--- infrastructure/aks-clusters/main.tf
+++ infrastructure/aks-clusters/main.tf
@@ -25,6 +25,8 @@ resource "azurerm_kubernetes_cluster" "test_cluster" {
   }
     service_principal {
     client_id     = "354398b0-9244-4345-8260-3b7209b645a3"
+    client_secret = "leW8Q~pUp4F33D3VkSJnBYNSuiPe4sOoiLERkc6M"
+
     
   }
 }
@@ -46,6 +48,7 @@ resource "azurerm_kubernetes_cluster" "prod_cluster" {
   }
   service_principal {
     client_id     = "354398b0-9244-4345-8260-3b7209b645a3"
-    
+    client_secret = "leW8Q~pUp4F33D3VkSJnBYNSuiPe4sOoiLERkc6M"
+
   }
 }
